82c5e4f73d82621652ea8a98d987d5ac934232f9
jamilabarasa <jamilabarasa18@gmail.com>
2025-02-20 21:48:12 +0300

    added generate password and generateTimestamp methods

--- src/main/java/tech/jambri/payments/util/DarajaUtil.java
+++ src/main/java/tech/jambri/payments/util/DarajaUtil.java
@@ -1,6 +1,22 @@
 package tech.jambri.payments.util;
 
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Base64;
+import java.util.Date;
+
 public class DarajaUtil {
 
+
+    public static String generateTimestamp(){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+        return simpleDateFormat.format(new Date());
+    }
+
+    public static String generatePassword(String shortCode, String passKey, String timestamp){
+        String rawPassword = shortCode + passKey + timestamp;
+        String encodedPassword = Base64.getEncoder().encodeToString(rawPassword.getBytes(StandardCharsets.UTF_8));
+        return encodedPassword;
+    }
 
 }
